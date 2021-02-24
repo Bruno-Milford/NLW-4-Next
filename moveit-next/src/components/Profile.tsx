ee3f35cc24213277165ac6bd6388034951309550
Trynda-LOL <bm33518@gmail.com>
2021-02-24 01:28:31 -0300

    Initial profile and module css

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+    return(
+        <div>
+            <img src="https://github.com/Bruno-Milford" alt="Bruno Milford"/>
+            <div>
+                <strong>Bruno Milford</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
